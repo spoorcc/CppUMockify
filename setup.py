07bd5144d94ae3444a6c019abd9297367816dab7
ben <ben.spoor@gmail.com>
2018-06-01 22:20:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='CppUMockify',
+    version='0.0.1',
+    description='Generate CppUMock implementations',
+    long_description=long_description,
+    url='https://github.com/spoorcc/cppumockify',
+    author='spoorcc',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='cpputest cppumock pycparser',
+
+    py_modules=["cppumockify"],
+    install_requires=['pycparser'],
+
+    entry_points={
+        'console_scripts': [
+            'cppumockify=cppumockify:main',
+        ],
+    },
+
+    project_urls={
+        'Bug Reports': 'https://github.com/spoorcc/cppumockify/issues',
+        'Source': 'https://github.com/spoorcc/cppumockify/',
+    },
+)
