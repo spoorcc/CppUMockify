6b41671adeb560be0e52f7a7132a9ab9739ebfd4
ben <ben.spoor@gmail.com>
2018-06-02 13:42:03 +0200

    Github showed me license is ISCL, respect the orginal authors choice...

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
