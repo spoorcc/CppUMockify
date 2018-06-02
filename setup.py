4cdec4cb74a274b369a9b1977a73602db304174d
ben <ben.spoor@gmail.com>
2018-06-02 13:42:48 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='CppUMockify',
-    version='0.0.4',
+    version='0.0.5',
     description='Generate CppUMock implementations',
     long_description=long_description,
     long_description_content_type='text/markdown',
