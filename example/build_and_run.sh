e56702bfb3dd22b9680336533151d3e4a11e6293
ben <ben.spoor@gmail.com>
2018-06-03 09:35:51 +0200

    Only create example_venv when not in TRAVIS build

--- example/build_and_run.sh
+++ example/build_and_run.sh
@@ -3,9 +3,11 @@
 # Change to script directory
 cd "$(dirname "$0")"
 
-# Create a virtual environment
-python -m venv example_venv
-. example_venv/bin/activate
+# Create a virtual environment (when not in travis)
+if [ -z "$TRAVIS" ]; then
+    python -m venv example_venv
+    . example_venv/bin/activate
+fi
 
 # Install  cppumockify
 pip install -e ..
