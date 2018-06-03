59a8446e0e2bc744b1eef7930e5958b6666097eb
ben <ben.spoor@gmail.com>
2018-06-03 09:04:24 +0200

    Build and run example in travis build

---
+++ example/build_and_run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+# Change to script directory
+cd "$(dirname "$0")"
+
+# Create a virtual environment
+python -m venv example_venv
+. example_venv/bin/activate
+
+# Install  cppumockify
+pip install -e ..
+
+# Build the example project
+mkdir -p bld
+cd bld
+cmake ..
+make
+
+# Run the example test
+./test_greetings
+
+
